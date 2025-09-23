30f0129a7cfa5c4422729be4144bb485e7b0b884
Raniwis <rosemary.9816@hotmail.com>
2025-09-23 18:08:17 -0300

    minor comment and build files removed

--- bioseq_dl/gui/app.py
+++ bioseq_dl/gui/app.py
@@ -1,6 +1,13 @@
 import gradio as gr
 from .main_ui import build_ui
 
+"""
+Gradio app for BioSeqDownloader
+
+Run with:
+    bioseq-dl gui run
+
+"""
 def main(host="127.0.0.1", port=7860, share=False):
     demo = build_ui()
     demo.launch(server_name=host, server_port=port, share=share)
